4fe4c5334ab66a5634c812704ffbb5852255b59b
Rushabh <rushabhmistry16@gmail.com>
2024-12-24 23:49:51 +0530

    UI Updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React, {useState, useEffect } from 'react';
 import { Link } from 'react-scroll'; 
 import { ethers } from 'ethers';
 import Image from './assets/logo.png';
-import { Github, CopyCheck, Copy } from 'lucide-react'; 
+import { CopyCheck, Copy } from 'lucide-react'; 
 import { motion } from 'framer-motion';
 
-import DemoComponent from './DemoComponent';
+import DemoComponent from "./DemoComponent";
 
 function App() {
-
   const [isCopied, setIsCopied] = useState(false);
   const [typedText, setTypedText] = useState("");
   const frameworks = ["React", "Vue.js", "Next.js", "Gatsby", "Svelte", "Angular"];
@@ -16,9 +15,9 @@ function App() {
   const [charIndex, setCharIndex] = useState(0);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText('npm install web3-react-hooks');
+    navigator.clipboard.writeText("npm install web3-react-hooks");
     setIsCopied(true);
-    setTimeout(() => setIsCopied(false), 2000); 
+    setTimeout(() => setIsCopied(false), 2000);
   };
 
   useEffect(() => {
@@ -38,10 +37,8 @@ function App() {
     return () => clearInterval(typingEffect);
   }, [charIndex, frameworks, index]);
 
-
   return (
     <div className="bg-gradient-to-r from-gray-900 to-black text-white font-poppins">
-
       <motion.nav
         className="backdrop-blur-md text-white fixed top-0 w-full z-50 shadow-lg transition-all duration-300"
         id="navbar"
@@ -52,7 +49,12 @@ function App() {
         <div className="container mx-auto py-4 flex justify-between items-center">
           {/* Logo and Project Name on the Left */}
           <div className="flex items-center space-x-3">
-            <img src={Image} alt="Logo" className="w-16 h-16 object-cover rounded-full" />
+            <img
+              src={Image}
+              alt="Logo"
+              className="w-16 h-16 object-cover rounded-full"
+              aria-label="Web3Connect Logo"
+            />
             <h1 className="text-2xl font-bold">Web3Connect</h1>
           </div>
 
@@ -63,6 +65,7 @@ function App() {
               smooth={true}
               duration={500}
               className="hover:text-[#00bfff] transition duration-300 cursor-pointer font-poppins"
+              aria-label="Navigate to Features Section"
             >
               Features
             </Link>
@@ -71,6 +74,7 @@ function App() {
               smooth={true}
               duration={500}
               className="hover:text-[#1e90ff] transition duration-300 cursor-pointer"
+              aria-label="Navigate to Demonstration Section"
             >
               Demonstration
             </Link>
@@ -79,131 +83,113 @@ function App() {
               smooth={true}
               duration={500}
               className="hover:text-[#1e90ff] transition duration-300 cursor-pointer"
+              aria-label="Navigate to Code Samples Section"
             >
               Code Samples
             </Link>
-
-          <a
-            href="https://github.com/Rushhaabhhh/Web3ReactHooks"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-[#1e90ff] transition duration-300 cursor-pointer"
+            <a
+              href="https://github.com/Rushhaabhhh/Web3ReactHooks"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-[#1e90ff] transition duration-300 cursor-pointer"
+              aria-label="Visit Web3 React Hooks GitHub Repository"
             >
-             GitHub
-          </a>
+              GitHub
+            </a>
           </div>
-
-
-
-          
         </div>
       </motion.nav>
 
-      {/* Hero Section with transparent background */}
+      {/* Hero Section */}
       <div className="py-40 bg-transparent" id="home">
-      <div className="container mx-auto px-6 flex justify-between items-center">
-        {/* Left side: Texts */}
-        <div className="flex-1">
-          <motion.h1
-            className="text-5xl font-bold mb-4"
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-          >
-            Engineered from a Pain Point, Introducing to You
-          </motion.h1>
-          <motion.h2
-            className="text-6xl font-extrabold text-indigo-600 mb-6"
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1, type: "spring", stiffness: 100 }}
-          >
-            <motion.span
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              className="relative inline-block"
+        <div className="container mx-auto px-6 flex justify-between items-center">
+          <div className="flex-1">
+            <motion.h1
+              className="text-5xl font-bold mb-4"
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
             >
-              'Web3 React Hooks'
-              <span className="absolute -bottom-2 left-0 w-full h-1 bg-indigo-600 transition-all duration-500 transform scale-x-0 origin-left group-hover:scale-x-100"></span>
-            </motion.span>
-          </motion.h2>
-          <motion.h3
-            className="text-3xl font-semibold mb-6"
-            initial={{ opacity: 0, x: -50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, delay: 0.5 }}
-          >
-
-            <span>A Simple Solution of Web3 Integration</span> <br/>
-            <span>for All React Frameworks</span>
-          </motion.h3>
-
-          {/* Typing Animation for Frameworks */}
-          <motion.div
-            className="text-6xl text-gray-300 mt-4 mb-20"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1 }}
-          >
-            <span>{typedText}</span>
-            <span className="text-transparent">|</span>
-          </motion.div>
-
-          {/* Left-side paragraph beneath the title */}
-          <motion.p
-            className="text-lg text-gray-400 mt-6"
-            initial={{ opacity: 0, x: -50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, delay: 1 }}
-          >
-            Web3 React Hooks offer a simple and efficient way to manage Web3 interactions in your React applications. With hooks tailored to streamline blockchain connectivity and Ethereum interactions, you can integrate decentralized technology into your apps effortlessly and without the hassle.
-          </motion.p>
-        </div>
-
-
-
-        {/* Right side: Code Snippet */}
-        <div className="flex-1 text-center">
-          <motion.div
-            className="bg-gray-800 text-white p-6 rounded-lg shadow-lg py-10 w-3/4 mx-auto"
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.8 }}
-          >
-            <span className="text-xl block mb-4">Install with npm : </span>
-            <div className="flex items-center justify-center space-x-4">
-              <code className="bg-gray-900 text-white p-4 rounded-lg text-lg">
-                npm install web3-react-hooks
-              </code>
-              <button
-                onClick={handleCopy}
-                className="flex items-center text-indigo-500 hover:text-indigo-700 p-2"
+              Engineered from a Pain Point, Introducing to You
+            </motion.h1>
+            <motion.h2
+              className="text-6xl font-extrabold text-indigo-600 mb-6"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1, type: "spring", stiffness: 100 }}
+            >
+              <motion.span
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="relative inline-block"
               >
-                {isCopied ? (
-                  <motion.div
-                    initial={{ opacity: 0, scale: 0 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5 }}
-                  >
-                    <CopyCheck size={20} />
-                  </motion.div>
-                ) : (
-                  <motion.div
-                    initial={{ opacity: 0, scale: 0 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5 }}
-                  >
-                    <Copy size={20} />
-                  </motion.div>
-                )}
-              </button>
-            </div>
-          </motion.div>
+                'Web3 React Hooks'
+              </motion.span>
+            </motion.h2>
+            <motion.h3
+              className="text-3xl font-semibold mb-6"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.5 }}
+            >
+              <span>A Simple Solution of Web3 Integration</span>
+              <br />
+              <span>for All React Frameworks</span>
+            </motion.h3>
+
+            {/* Typing Animation */}
+            <motion.div
+              className="text-6xl text-gray-300 mt-4 mb-20"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 1 }}
+            >
+              <span>{typedText}</span>
+              <span className="text-transparent">|</span>
+            </motion.div>
+          </div>
+
+          {/* Right Side Code Snippet */}
+          <div className="flex-1 text-center">
+            <motion.div
+              className="bg-gray-800 text-white p-6 rounded-lg shadow-lg py-10 w-3/4 mx-auto"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.8 }}
+            >
+              <span className="text-xl block mb-4">Install with npm:</span>
+              <div className="flex items-center justify-center space-x-4">
+                <code className="bg-gray-900 text-white p-4 rounded-lg text-lg">
+                  npm install web3-react-hooks
+                </code>
+                <button
+                  onClick={handleCopy}
+                  className="flex items-center text-indigo-500 hover:text-indigo-700 p-2"
+                  aria-label="Copy npm install command"
+                >
+                  {isCopied ? (
+                    <motion.div
+                      initial={{ opacity: 0, scale: 0 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      transition={{ duration: 0.5 }}
+                    >
+                      <CopyCheck size={20} />
+                    </motion.div>
+                  ) : (
+                    <motion.div
+                      initial={{ opacity: 0, scale: 0 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      transition={{ duration: 0.5 }}
+                    >
+                      <Copy size={20} />
+                    </motion.div>
+                  )}
+                </button>
+              </div>
+            </motion.div>
+          </div>
         </div>
       </div>
-    </div>
-
-
             {/* Features Section */}
             <section className="container mx-auto px-6 py-20" id="features">
             <div className="text-center mb-16">
